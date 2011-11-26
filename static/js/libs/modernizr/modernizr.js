057567f2082ffcf8a2ce939d3bb25f2bdcb33009
Kevin Wang <kev.y.wang@gmail.com>
2011-11-26 15:29:26 -0500

    removed unnecessary libs

--- static/js/libs/modernizr/modernizr.js
+++
@@ -1,3 +0,0 @@
-define(['order!libs/modernizr/modernizr-min'], function(){
-  return window.Modernizr;
-});
--- static/js/main.js
+++ static/js/main.js
@@ -15,12 +15,9 @@ require([
   // Some plugins have to be loaded in order due to their non AMD compliance
   // Because these scripts are not "modules" they do not pass any values to the definition function below
   'order!libs/jquery/jquery-min',
-  'order!libs/jquery/jquery.tablesorter.min',
-  'order!libs/prettify/prettify',
   'order!libs/bootstrap/bootstrap-dropdown',
   'order!libs/bootstrap/bootstrap-twipsy',
   'order!libs/bootstrap/bootstrap-scrollspy',
-  'order!libs/bootstrap/bootstrap-modal',
   'order!libs/underscore/underscore-min',
 ], function(App) {
   App.initialize();
