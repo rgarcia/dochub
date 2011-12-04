d5ccf7f6fe528d7664067bd6ef9f0a69e0e4a9e5
Kevin Wang <kev.y.wang@gmail.com>
2011-12-04 15:47:41 -0600

    Fixed permalinks I just broke

--- static/js/router.js
+++ static/js/router.js
@@ -77,6 +77,7 @@ define([
             console.log('ROUTING ' + this.languageName + ' : ' + query);
             self.renderTopNav();  // _.once'd
             self.topNavView.setActiveElement('nav-' + this.languageName);
+            self.languageViews[this.languageName].setQuery(query);
           }
         };
         _.bindAll(cb, 'fn');
