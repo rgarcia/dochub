daabea21713ec8d9719d8159cfafdecc77702a87
Rafael Garcia <rgarcia2009@gmail.com>
2011-12-10 17:05:27 -0600

    rewrite of mdn css scraper
    - writes to data/css-mdn.json in static directory instead of db
    - fills in a ton of gaps in current scrape (509 items vs 300ish)

---
+++ scraper/css-mdn/scrape.js
@@ -0,0 +1,108 @@
+var requirejs = require('requirejs');
+
+requirejs([
+  'step',
+  'spider',
+  'underscore',
+  'cheerio',
+  '../../models/sectionscrape',
+  'path',
+  'fs'
+], function(step, spider, _, cheerio, SectionScrape, path, fs) {
+
+  var spidey = spider();
+
+  // use this to visit all links on a page
+  var visitLinks = function($) {
+    $('a').each(function() {
+      var href = $(this).attr('href');
+      spidey.get(href);
+    });
+  };
+
+  // file where we'll dump the json
+  var filename = path.dirname(__filename) + '/../../static/data/css-mdn.json';
+  console.log('dumping to ' + filename);
+  var file = fs.openSync(filename,'w');
+
+  // main index of mdn's css docs
+  spidey.route('developer.mozilla.org', '/en/CSS_Reference', function ($) {
+    visitLinks($);
+  });
+
+  var blacklist = [
+    'https://developer.mozilla.org/en/CSS/CSS_Reference'
+    , 'https://developer.mozilla.org/en/CSS/CSS_Reference/Property_Template'
+  ];
+
+  // some urls redirect to other pages w/o changing the url (for example: https://developer.mozilla.org/en/CSS/-moz-scrollbars-none)
+  // so in addition to not visiting the same url twice, keep this list to prevent visiting the same title twice
+  var titles = [];
+
+  spidey.route('developer.mozilla.org', '/en/CSS/*', function ($, url) {
+    try {
+
+    if ( _.indexOf(blacklist,url) !== -1 ) return;
+    visitLinks($);
+
+    console.log('---------');
+    console.log('scraping:',url);
+
+    var scrapeData = new SectionScrape();
+
+    var title = $('article .page-title h1').text();
+    if ( title === '' || title === null ) {
+      console.log('ERROR: could not get title, skipping');
+      return;
+    } else if ( _.indexOf(titles,title) !== -1 ) {
+      console.log('WARNING: already scraped something with this title, skipping');
+      return;
+    }
+
+    console.log('title:',title);
+
+    scrapeData['title'] = title;
+    scrapeData['url'] = url;
+    scrapeData['sectionNames'] = [];
+    scrapeData['sectionHTMLs'] = [];
+
+    // get all section ids
+    var ids = _.map($('[id^=section_]'), function(div) { return div.attribs.id } );
+    if ( ids.length === 0 ) {
+      console.log('WARNING: no sections...');
+      return;
+    }
+
+    for ( var i = 0; i < ids.length; i++ ) {
+      // load the section html as its own jquery object
+      var $section = cheerio.load($('[id^=' + ids[i] + ']').html());
+
+      // strip scripts
+      $section('script').remove();
+      var sectionName = "";
+
+      // TODO find relative hrefs and turn them into absolute hrefs
+
+      // find the title of the section--mdn isn't very consistent with what size headers they use
+      _.each([1,2,3,4],function(h) {
+        var headers = $section('h' + h);
+        if ( sectionName === "" && headers.length > 0 ) {
+          sectionName = headers.text();
+        }
+      });
+
+      scrapeData['sectionNames'].push(sectionName);
+      scrapeData['sectionHTMLs'].push($section.html());
+    }
+
+      fs.writeSync(file,JSON.stringify(scrapeData) + '\n',scrapeData);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  // start 'er up
+  spidey.get('https://developer.mozilla.org/en/CSS_Reference').log('info');
+
+  return;
+});
