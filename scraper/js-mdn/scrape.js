9c9748d12e75cd40bccef896a0f3aa267fad0c1c
Rafael Garcia <rgarcia2009@gmail.com>
2011-12-10 17:56:13 -0600

    ditto for js

--- scraper/js-mdn/scrape.js
+++ scraper/js-mdn/scrape.js
@@ -1,134 +1,102 @@
 var requirejs = require('requirejs');
 
 requirejs([
-  '../../config',
   'step',
   'spider',
-  'mongoose',
-  'underscore',   // used by mongoose.save
-  'cheerio',      // used by mongoose.save
-  '../../models/mdnjsobj',
-], function(config, step, spider, mongoose, _, cheerio, MDNJsObj) {
+  'underscore',
+  'cheerio',
+  '../../models/sectionscrape',
+  'path',
+  'fs'
+], function(step, spider, _, cheerio, SectionScrape, path, fs) {
 
-  // Config that shiz
-  var jsSpider = spider();
-  jsSpider.route('developer.mozilla.org', '/en/JavaScript/Reference', function ($) {
-    var crawlfn = function() {
+  var spidey = spider();
+
+  // use this to visit all links on a page
+  var visitLinks = function($) {
+    $('a').each(function() {
       var href = $(this).attr('href');
-      jsSpider.get(href);
-    };
-    $('#section_2 a').each(crawlfn);
-    $('#section_4 a').each(crawlfn);
-    $('#section_5 a').each(crawlfn);
+      spidey.get(href);
+    });
+  };
+
+  // file where we'll dump the json
+  var filename = path.dirname(__filename) + '/../../static/data/js-mdn.json';
+  console.log('dumping to ' + filename);
+  var file = fs.openSync(filename,'w');
+
+  // main index of mdn's js docs
+  spidey.route('developer.mozilla.org', '/en/JavaScript/Reference', function ($) {
+    visitLinks($);
   });
 
-  var subroutes = [
-    'JavaScript/Reference/Global_Objects',
-    'JavaScript/Reference/Statements',
-    'JavaScript/Reference/Operators',
-    'JavaScript_typed_arrays',
+  var blacklist = [
+    'https://developer.mozilla.org/en/JavaScript/Reference'
   ];
-  for (var i = 0; i < subroutes.length; ++i) {
-    // Need this closure to get the subroute
-    (function() {
-      var subroute = subroutes[i] + '/';
-      jsSpider.route('developer.mozilla.org', '/en/' + subroute + '*', function ($, url) {
-        var title = $('#title').text().trim();
-        if (title === '') return;
 
-        // Extract everything after the subroute
-        var remaining = url.split(subroute)[1];
-        var objName = remaining.replace(/\//g, '.');
-        console.log('[Scraping ' + url + ' :: ' + objName + ']');
+  // some urls redirect to other pages w/o changing the url (for example: https://developer.mozilla.org/en/CSS/-moz-scrollbars-none)
+  // so in addition to not visiting the same url twice, keep this list to prevent visiting the same title twice
+  var titles = [];
 
-        // Create new obj to save
-        var mdnobj = new MDNJsObj();
-        step(
-          function searchForExistingDoc() {
-            MDNJsObj.findOne({ fullTitle: objName }, this);
-          },
+  spidey.route('developer.mozilla.org', '(\/en\/JavaScript_typed_arrays|\/en\/JavaScript\/Reference\/(Global_Objects|Statement|Operators))\/*', function ($, url) {
+    if ( _.indexOf(blacklist,url) !== -1 ) return;
+    visitLinks($);
 
-          function processFind(err, doc) {
-            if (err) {
-              console.log('[processFind error: ' + err + ']');
-              throw err;
-            }
+    console.log('---------');
+    console.log('scraping:',url);
 
-            if ( !doc ) {
-              console.log('[No existing doc, creating one.]');
-              return null;
-            } else {
-              console.log('[Removing doc and creating a new one.]');
-              doc.remove(this);
-            }
-          },
+    var scrapeData = new SectionScrape();
 
-          function processRemoveAndAddNewDoc(err) {
-            if (err) {
-              console.log('[processRemoveAndAddNewDoc error: ' + err + ']');
-              throw err;
-            }
-
-            mdnobj['title']     = title;
-            mdnobj['fullTitle'] = objName;
-            mdnobj['sectionNames'] = [];
-            mdnobj['sectionHTMLs'] = [];
-
-            var ids = _.map($('[id^=section_]'), function(div) { return div.attribs.id } );
-
-            for ( var i = 0; i < ids.length; i++ ) {
-              var $section = cheerio.load($('[id^=' + ids[i] + ']').html());
-              $section('script').remove(); // strip scripts
-              var sectionName = "";
-              for ( var j = 1; j <= 4; j++ ) {
-                var headers = $section('h' + j);
-                if ( headers.length > 0 ) {
-                  sectionName = headers.text();
-                  break;
-                }
-              }
-              mdnobj['sectionNames'].push(sectionName);
-              mdnobj['sectionHTMLs'].push($section.html());
-            }
-
-            mdnobj.save(this);
-          },
-
-          function postDBSave(err) {
-            if (err) {
-              console.log('[ERROR could not save to db: ' + err + ']');
-              throw err;
-            }
-
-            console.log('[Successfully saved to db.]');
-            return null;
-          },
-
-          function continueCrawling() {
-            var crawlfn = function() {
-              var href = $(this).attr('href');
-              jsSpider.get(href);
-            };
-            $('#pageText a').each(crawlfn);
-          }
-        );
-      });
-    })();
-  }
-
-  // Do it son
-  console.log('[Connecting to db: ' + config.mongo_uri + ']');
-  mongoose.connect(config.mongo_uri, function(err) {
-    if (err) {
-      console.log('[Error connecting to db: ' + err + ']');
-      throw err;
-    } else {
-      console.log('[Connected to ' + config.mongo_uri + ']');
+    var title = $('article .page-title h1').text();
+    if ( title === '' || title === null ) {
+      console.log('ERROR: could not get title, skipping');
+      return;
+    } else if ( _.indexOf(titles,title) !== -1 ) {
+      console.log('WARNING: already scraped something with this title, skipping');
+      return;
     }
 
-    jsSpider.get('https://developer.mozilla.org/en/JavaScript/Reference').log('info');
+    console.log('title:',title);
+
+    scrapeData['title'] = title;
+    scrapeData['url'] = url;
+    scrapeData['sectionNames'] = [];
+    scrapeData['sectionHTMLs'] = [];
+
+    // get all section ids
+    var ids = _.map($('[id^=section_]'), function(div) { return div.attribs.id } );
+    if ( ids.length === 0 ) {
+      console.log('WARNING: no sections...');
+      return;
+    }
+
+    for ( var i = 0; i < ids.length; i++ ) {
+      // load the section html as its own jquery object
+      var $section = cheerio.load($('[id^=' + ids[i] + ']').html());
+
+      // strip scripts
+      $section('script').remove();
+      var sectionName = "";
+
+      // TODO find relative hrefs and turn them into absolute hrefs
+
+      // find the title of the section--mdn isn't very consistent with what size headers they use
+      _.each([1,2,3,4],function(h) {
+        var headers = $section('h' + h);
+        if ( sectionName === "" && headers.length > 0 ) {
+          sectionName = headers.text();
+        }
+      });
+
+      scrapeData['sectionNames'].push(sectionName);
+      scrapeData['sectionHTMLs'].push($section.html());
+    }
+
+    fs.writeSync(file,JSON.stringify(scrapeData) + '\n',scrapeData);
   });
+
+  // start 'er up
+  spidey.get('https://developer.mozilla.org/en/JavaScript/Reference').log('info');
 
   return;
 });
-
