92db8f39e4e528e1d60cbb6acea2441ae8e2afcf
Rafael Garcia <rgarcia2009@gmail.com>
2011-12-12 20:19:32 -0600

    whoops, forgot this

---
+++ static/js/models/sectionscrape.js
@@ -0,0 +1,36 @@
+define([
+  'jQuery',
+  'Underscore',
+  'Backbone'
+], function($, _, Backbone) {
+
+  var SectionScrape = Backbone.Model.extend({
+
+    idAttribute: '_id',
+
+    defaults: {
+      'title'                : '',
+      'htmlEscapedTitle'     : '', // Set in initialize
+      'url'                  : '',
+      'sectionNames'         : [],
+      'sectionHTMLs'         : [],
+
+      // display properties
+      'tocVisible'           : false,
+      'mainVisible'          : false,
+    },
+
+    initialize: function() {
+      var htmlEscapedTitle = $('<div/>').html(this.get('title')).text();
+      this.set({
+        htmlEscapedTitle: htmlEscapedTitle,
+        lowerCaseTitle: htmlEscapedTitle.toLowerCase()
+      }, {
+        silent: true
+      });
+    },
+
+  });
+
+  return SectionScrape;
+});
