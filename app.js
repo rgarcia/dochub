dfd483509b8c84a55f0e59fb00f434cde70b5349
Rafael Garcia <rgarcia2009@gmail.com>
2011-12-23 10:48:37 -0600

    adding application cache
    seems to work when viewing locally, i.e. on closing and re-opening the window the only request that hits the server is for the cache manifest file.
    
    TODO: form the cache manifest file programmatically. right now every time we add a file we need to be careful to add it to the cache, which kinda sucks.

--- app.js
+++ app.js
@@ -4,12 +4,30 @@ define([
   // 'controllers/loader',
   'module',
   'path',
-  './config'
-], function (express,/* mongoose, controllerLoader,*/ module, path, config) {
+  './config',
+  'fs'
+], function (express,/* mongoose, controllerLoader,*/ module, path, config, fs) {
 
   var app = null;
 
   console.log(global.process.env);
+
+  // can't serve cache manifest w/ express static since it doesn't set the header
+  // correctly. so we'll load the file once, keep it in memory, serve it up manually
+  var filename = module.uri;
+  var manifest = null;
+  fs.readFile(path.dirname(filename) + '/static/offline.manifest', function(err,buf) {
+    if(err)
+      throw(err);
+    manifest = {
+      headers:{
+        'Content-Type'  : 'text/cache-manifest',
+        'Content-Length': buf.length,
+        // 'Cache-Control' : 'public, max-age=' + 60*60 // do we need this caching?
+      },
+      body: buf
+    };
+  });
 
   return {
     initialize: function() {
@@ -27,7 +45,11 @@ define([
         //     console.log('connected to ' + config.mongo_uri);
         // });
         app.use(express.logger({ format: ':method :url :status' }));
-        var filename = module.uri;
+        // preempt static to serve up cache manifest
+        app.get("/offline.manifest", function(req, res){
+          res.writeHead(200,manifest.headers);
+          res.end(manifest.body);
+        });
         var staticDir = path.dirname(filename) + '/static';
         console.log('initializing static: ' + staticDir);
         app.use(express.static(staticDir));
