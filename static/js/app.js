21d6f2e22d2e134962f1278ae0d545b299abbe99
Kevin Wang <kev.y.wang@gmail.com>
2012-01-15 15:56:19 -0600

    Fixed external links in Python docs

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,15 @@ define([
   'router',
 ], function($, _, Backbone, Router) {
   var initialize = function() {
+    // c.f. http://appcachefacts.info/
+    if (window.applicationCache) {
+      applicationCache.addEventListener('updateready', function() {
+        if (confirm('An update is available. Reload now?')) {
+          window.location.reload();
+        }
+      });
+    }
+
     Router.initialize();
   }
 
